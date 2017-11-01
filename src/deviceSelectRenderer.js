489b33edb4191e300720a05facfe88b3a594e2e0
naresh krishnamoorthy <naresh.ideas2it@gmail.com>
2017-11-01 13:17:26 +0530

    Code comments and future TODOs

--- src/deviceSelectRenderer.js
+++ src/deviceSelectRenderer.js
@@ -1,69 +1,108 @@
 const remote = require('electron').remote;
 window.$ = window.jQuery = require("jquery");
-const { createLogger, format, transports } = require('winston');
-
+const {
+  createLogger,
+  format,
+  transports
+} = require('winston');
 const logger = createLogger({
   level: 'info',
   format: format.json(),
   transports: [
     //
-    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-	new transports.Console(),
-	new transports.File({ filename: './logs/debug.log', level: 'debug' }),
-    new transports.File({ filename: './logs/error.log', level: 'error' }),
-    new transports.File({ filename: './logs/combined.log' })
+    new transports.Console(),
+    new transports.File({
+      filename: './logs/debug.log',
+      level: 'debug'
+    }),
+    new transports.File({
+      filename: './logs/error.log',
+      level: 'error'
+    }),
+    new transports.File({
+      filename: './logs/combined.log'
+    })
   ]
 });
 
-var ipcRenderer = require('electron').ipcRenderer; 
-var selectedDevices = [];
+var ipcRenderer = require('electron').ipcRenderer; //IP renderer object to talk to the main process
+var selectedDevices = []; //array of selected devices
 
- ipcRenderer.on("devices_list", function(event, arg){
-	 logger.log('debug', "In device select window :: " + arg);
-	 showDevicesList(arg);
- });
+/**********************
+UI Section (user clicks, hovers etc)
+**********************/
 
-$('#div_app_close').click(function(){
-	 var window = remote.getCurrentWindow();
-     window.close();
+/*
+Close this window when the user click the top right 'X' button
+ */
+$('#div_app_close').click(function() {
+  ipcRenderer.send("accept_selection", []);
+  var window = remote.getCurrentWindow();
+  window.close();
 });
 
- $("#div_reset").click(function(){
-	logger.info('info', "User selected to reset device selection UI.")
-	selectedDevices = [];
- })
+/*
+  reset the UI when the user click to re-define his device selection
+ */
+$("#div_reset").click(function() {
+  logger.info('info', "User selected to reset device selection UI.")
+  selectedDevices = [];
+})
 
-function showDevicesList(devices){
-	var content="";
-	devices.forEach(function(el, index){
-		content += '<li class="list-group-item"> <span class="icon icon-monitor"></span><div class="media-body"><strong>' + el.ip + ' : ' + el.port + '</strong><p>Device description here.</p></div></li>'
-	});
-	logger.log('debug', "UI shows device[s] :: " + content);
-	$(".list-group").html(content);
-	$(".list-group > li").click(function(){
-		var i = $(this).index();
-		logger.log('info', "User selected :: " + i);
-		$(this).toggleClass("list-group-item-selected")
-		if($(this).hasClass("list-group-item-selected")){
-			selectedDevices.push(i);
-		}else{
-			selectedDevices.splice(selectedDevices.indexOf(i), 1);
-		}
-		//console.log(selectedDevices);
-	});
+/*
+Accept the selection made by the user and intimate the main app window
+ */
+$("#accept_selection").click(function() {
+  ipcRenderer.send("accept_selection", selectedDevices);
+  logger.log('info', "User selected devices ::: " + selectedDevices.length);
+});
+
+/*
+ reset the UI when the user click to re-define his device selection
+ */
+$("#reset_selection").click(function() {
+  logger.log('info', "User reset device selection");
+  $(".list-group > li").removeClass("list-group-item-selected");
+  selectedDevices = [];
+});
+
+/***************************
+Util funtions section
+***************************/
+
+/*
+Function to draw the devices into a list for the user to scroll and select.
+ */
+function showDevicesList(devices) {
+  var content = "";
+  devices.forEach(function(el, index) {
+    content += '<li class="list-group-item"> <span class="icon icon-monitor"></span><div class="media-body"><strong>' + el.ip + ' : ' + el.port + '</strong><p>Device description here.</p></div></li>'
+  });
+  logger.log('debug', "UI shows device[s] :: " + content);
+  $(".list-group").html(content);
+  $(".list-group > li").click(function() {
+    var i = $(this).index();
+    logger.log('info', "User selected :: " + i);
+    $(this).toggleClass("list-group-item-selected")
+    if ($(this).hasClass("list-group-item-selected")) {
+      selectedDevices.push(i);
+    } else {
+      selectedDevices.splice(selectedDevices.indexOf(i), 1);
+    }
+  });
 }
- 
-$("#accept_selection").click(function(){
-	ipcRenderer.send("accept_selection", selectedDevices);
-	logger.log('info', "User selected devices ::: " + selectedDevices.length );
-});
 
-$("#reset_selection").click(function(){
-	logger.log('info', "User reset device selection");
-	$(".list-group > li").removeClass("list-group-item-selected");
-	selectedDevices = [];
-}); 
- 
- 
+
+/**********************
+IPC section
+***********************/
+/*
+IPC to handle when the select devices screen receives the list of all devices from the main menu
+ */
+ipcRenderer.on("devices_list", function(event, arg) {
+  logger.log('debug', "In device select window :: " + arg);
+  showDevicesList(arg);
+});
--- src/main.js
+++ src/main.js
@@ -1,39 +1,51 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
-const path = require('path')
-const url = require('url')
-var {ipcMain} = electron; 
-
-const { createLogger, format, transports } = require('winston');
-
+const electron = require('electron') // Electron module
+const app = electron.app // Module to control application life.
+const BrowserWindow = electron.BrowserWindow // Module to create native browser window.
+const path = require('path') // Module for accessing file paths
+const url = require('url') // Module for loading the url from links (local or web)
+var {
+  ipcMain
+} = electron; // Main module IPC object
+const {
+  createLogger,
+  format,
+  transports
+} = require('winston'); // Node library to enable efficient logging in apps instead of the 'dreaded' console logs 8P
 const logger = createLogger({
   level: 'info',
   format: format.json(),
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log` 
-    // - Write all logs error (and below) to `error.log`.
-    //
-	new transports.Console(),
-	new transports.File({ filename: './logs/debug.log', level: 'debug' }),
-    new transports.File({ filename: './logs/error.log', level: 'error' }),
-    new transports.File({ filename: './logs/combined.log' })
+    new transports.Console(),
+    new transports.File({
+      filename: './logs/debug.log',
+      level: 'debug'
+    }),
+    new transports.File({
+      filename: './logs/error.log',
+      level: 'error'
+    }), // Write all logs error (and below) to `error.log`.
+    new transports.File({
+      filename: './logs/combined.log'
+    }) // Write to all logs with level `info` and below to `combined.log`
   ]
 });
 
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-let deviceSelectWindow
+let mainWindow;
+let deviceSelectWindow;
 
-function createWindow () {
+/*
+Function to create the main windows for the DashUploader app
+ */
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 300, frame: false})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 300,
+    frame: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -41,26 +53,35 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }))
-  
+
   mainWindow.setResizable(false);
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-	logger.log('debug', "App was closed");
+    logger.log('debug', "App was closed");
     mainWindow = null
   })
 
 }
 
+/*
+Function to create the device selection window for the Dashuploader app
+ */
 function createSelectDeviceWindow(devices) {
   // Create the browser window.
-  deviceSelectWindow = new BrowserWindow({width: 400, height: 800, frame: false, parent: mainWindow, modal: true})
+  deviceSelectWindow = new BrowserWindow({
+    width: 400,
+    height: 800,
+    frame: false,
+    parent: mainWindow,
+    modal: true
+  })
 
   // and load the index.html of the app.
   deviceSelectWindow.loadURL(url.format({
@@ -68,25 +89,25 @@ function createSelectDeviceWindow(devices) {
     protocol: 'file:',
     slashes: true
   }))
-  
+
   deviceSelectWindow.webContents.on('did-finish-load', () => {
     logger.log("debug", "windows finsihed loading..sending info now");
-	deviceSelectWindow.webContents.send('devices_list', devices)
+    deviceSelectWindow.webContents.send('devices_list', devices)
   })
-  
+
   deviceSelectWindow.setResizable(false);
 
   // Open the DevTools.
   //deviceSelectWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  deviceSelectWindow.on('closed', function () {
+  deviceSelectWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     deviceSelectWindow = null;
   })
-   
+
   deviceSelectWindow.show()
 }
 
@@ -97,7 +118,7 @@ function createSelectDeviceWindow(devices) {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -105,7 +126,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -116,21 +137,30 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 // Listen for async message from renderer process
-ipcMain.on('async', (event, arg) => {  
-    // Print 1
-    console.log("Got async message", arg);
-    // Reply on async message from renderer process
-    //event.sender.send('async-reply', 2);
+ipcMain.on('async', (event, arg) => {
+  // Print 1
+  console.log("Got async message", arg);
+  // Reply on async message from renderer process
+  //event.sender.send('async-reply', 2);
 });
 
-ipcMain.on('select_devices', (event, arg) => {  
+/****************************
+IPC Section
+*****************************/
+
+/*
+IPC message to process the event when user presses the select devices button
+ */
+ipcMain.on('select_devices', (event, arg) => {
   logger.log('info', "Launch select devices window" + JSON.stringify(arg));
   createSelectDeviceWindow(arg);
-  
 });
 
+/*
+IPC Message to process the event when the user has finalised his device selection 
+ */
 ipcMain.on('accept_selection', (event, arg) => {
-	logger.log('info', arg.length + " device(s) ready for firmware upload procedure.");
-	deviceSelectWindow.close();
-	mainWindow.webContents.send("devices_selected", arg);	
+  logger.log('info', arg.length + " device(s) ready for firmware upload procedure.");
+  deviceSelectWindow.close();
+  mainWindow.webContents.send("devices_selected", arg);
 });
